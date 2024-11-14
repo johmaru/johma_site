a8e46f4aab2a6c4bb514310dbce36af84d242bb7
Jo Matsumaru <hatunehimeka@outlook.jp>
2024-11-14 23:20:34 +0900

    home関係

--- johma_site/wwwroot/js/site.js
+++ johma_site/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function typeWriter(element, text, speed) {
+    let i = 0;
+  function type(){
+      if (i < text.length) {
+          element.innerHTML += text.charAt(i);
+          i++;
+          setTimeout(type, speed);
+      }
+  }
+    type();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const element = document.querySelector('.typewriter');
+    const text = element.getAttribute('data-text');
+   setTimeout(function () {
+        typeWriter(element, text, 100);
+    }, 3000);
+});
